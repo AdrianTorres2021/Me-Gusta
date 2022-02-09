867b4d698139a295b752899c80531042221306e6
Adrian Torres <adrianluistorres2@gmail.com>
2022-02-09 17:25:13 -0500

    Me gusta Finished

---
+++ script.js
@@ -0,0 +1,13 @@
+
+var likes = [9, 12, 9];
+var spans = [
+    document.querySelector(".count"),
+    document.querySelector(".count-2"),
+    document.querySelector(".count-3")
+
+];
+
+function like(id) {
+    likes[id]++;
+    spans[id].innerHTML = likes[id] + "like(s)";
+}
